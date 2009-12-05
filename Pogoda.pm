302c36e1cb7ab1cecfb1af4da4e10e452c6b2daf
Alex Kapranoff <kapranoff@gmail.com>
2009-12-05 19:41:51 +0300

    Integrate API tester in

--- Pogoda.pm
+++ Pogoda.pm
@@ -1,9 +1,38 @@
 package Pogoda;
 use Dancer;
 use Template;
+use Pogoda::Samples;
+use Pogoda::DB;
 
 get '/' => sub {
     template 'index';
+};
+
+before sub {
+    var dbc =>
+        Pogoda::DB->connect("dbi:SQLite:dbname=" . set('sqlite_file_test')); # XXX
+};
+
+get '/get_test_data' => sub {
+    my @samples = debug_all_samples(vars->{dbc});
+
+    template 'test_data' => { samples => \@samples };
+};
+
+get '/test_sample_form' => sub {
+    template 'sample_form.tt';
+};
+
+get '/post_sample' => sub {
+    eval {
+        add_sample(vars->{dbc}, params);
+    };
+    if ($@) {
+        status 500;
+    }
+    else {
+        return "OK";
+    }
 };
 
 true;
